2d38078fe0e482b8edd7d41e4adac5baf404f20f
Matthias Nuessler <m.nuessler@web.de>
2011-08-21 10:35:27 +0200

    Configure plugin versions and reports in POM. Add draft for report mojo.
    Signed-off-by: Matthias Nuessler <m.nuessler@web.de>

---
+++ src/main/java/org/nuessle/maven/plugins/cakupan/CakupanReportMojo.java
@@ -0,0 +1,57 @@
+package org.nuessle.maven.plugins.cakupan;
+
+import java.util.Locale;
+
+import org.apache.maven.doxia.sink.Sink;
+import org.apache.maven.doxia.siterenderer.Renderer;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import org.apache.maven.reporting.MavenReportException;
+
+/**
+ * @author mnuessler
+ * @goal myreport
+ * @phase site
+ */
+public class CakupanReportMojo extends AbstractMavenReport {
+
+	public String getOutputName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName(Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDescription(Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Renderer getSiteRenderer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getOutputDirectory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected MavenProject getProject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void executeReport(Locale locale) throws MavenReportException {
+		Sink sink = getSink();
+		sink.body();
+	}
+
+}
