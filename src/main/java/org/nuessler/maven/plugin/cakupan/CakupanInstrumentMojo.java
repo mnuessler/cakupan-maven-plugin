1e241e387e6b4b63fdc75cf0dc138a22d04e4627
Matthias Nuessler <m.nuessler@web.de>
2011-09-15 21:18:32 +0200

    Moved instrument mojo class.
    Signed-off-by: Matthias Nuessler <m.nuessler@web.de>

--- src/main/java/org/nuessler/maven/plugin/cakupan/CakupanInstrumentMojo.java
+++ src/main/java/org/nuessler/maven/plugin/cakupan/CakupanInstrumentMojo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.nuessler.maven.plugins.cakupan;
+package org.nuessler.maven.plugin.cakupan;
 
 import java.io.File;
 import java.util.Collections;
